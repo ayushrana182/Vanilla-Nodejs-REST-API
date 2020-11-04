d3797b17f88f62064c8b415c2e279793ddf9b9e6
Ayush Rana <ayushrana182@gmail.com>
2020-11-04 09:14:35 +0545

    Getting json products

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const {getProducts, getProduct} = require ('./controllers/productController')
+
+const server = http.createServer((req, res) => {
+  
+  if (req.url === '/api/products' && req.method === 'GET') {
+    getProducts(req, res)
+  } else if(req.url.match(/\/api\/products\/([0-9])/)&& req.method ==='GET')
+  {
+    const id = req.url.split('/') [3]
+    getProducts(req,res, id)
+  } 
+  else {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      message: 'Route Not Found'
+    }))
+  }
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
