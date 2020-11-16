94bcd3ea3a16f56e60edd607197e9b43c86dcbc6
Ayush Rana <ayushrana182@gmail.com>
2020-11-16 09:56:07 +0545

    Added Create Function(Products)

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require('http');
-const {getProducts, getProduct} = require ('./controllers/productController')
+const {getProducts, getProduct,createProduct} = require ('./controllers/productController')
 
 const server = http.createServer((req, res) => {
-  
   if (req.url === '/api/products' && req.method === 'GET') {
     getProducts(req, res)
   } else if(req.url.match(/\/api\/products\/([0-9])/)&& req.method ==='GET')
   {
     const id = req.url.split('/') [3]
-    getProducts(req,res, id)
-  } 
+    getProduct(req,res, id)
+  } else if(req.url === '/api/products' && req.method === 'POST'){
+    createProduct(req,res)
+  }
   else {
     res.writeHead(200, {
       'Content-Type': 'application/json'
