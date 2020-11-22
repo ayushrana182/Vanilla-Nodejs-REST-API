ff63b04a15c31f15647c12a6fb96e4dad3d0a1ea
Ayush Rana <ayushrana182@gmail.com>
2020-11-22 11:39:10 +0545

    Getting Body Data

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,61 +1,62 @@
-const Product = require('../models/productModel')
-
+const Product = require('../models/productModel');
+const { getPostData } = require('../utils');
 // @desc Gets All Products
 // @route GET/api/products
-async function getProducts(req, res){
-    try {
-        const products = await Product.findAll()
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
 
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-          })
-          res.end(JSON.stringify(products))
-    } catch (error) {
-        console.log(error)
-    }
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // @desc Gets Single Products
 // @route GET/api/product/:id
-async function getProduct(req, res, id){
-    try {
-        const product = await Product.findById(id)
-        if(!product){
-            res.writeHead(404, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify({message:'Product not found'}))
-        }else{
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify(product))
-
-        }
-        
-    } catch (error) {
-        console.log(error)
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product not found' }));
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(product));
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // @desc Create a Product
 // @route POST/api/products
-async function createProduct(req, res){
-    try {
-        const product = {
-            title: 'Test Product',
-            description: 'This is my product',
-            price: 100
-        }
+async function createProduct(req, res) {
+  try {
+    const body = await getPostData(req);
 
-        const newProduct = await Product.create(product)
+    const { title, description, price } = JSON.parse(body);
 
-        res.writeHead(201,{'Content-Type':'application/json'})
-        return res.end(JSON.stringify(newProduct))
-        
-    } catch (error) {
-        console.log(error)
-    }
+    const product = {
+      title,
+      description,
+      price,
+    };
+
+    const newProduct = await Product.create(product);
+
+    res.writeHead(201, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify(newProduct));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    getProducts,
-    getProduct,
-    createProduct
-}
+  getProducts,
+  getProduct,
+  createProduct,
+};
